f47b2d8538063f848692ba5e53a5804db346b4d5
bex2208 <beccymueller@hotmail.com>
2026-02-26 20:23:55 +0100

    Update Placeholder_Tests.py

--- Placeholder_Tests.py
+++ Placeholder_Tests.py
@@ -58,3 +58,21 @@ advice = "Jacket and waterproof boots. Maybe a hat if it rains."
 # ===== Telegram Bot =====
 bot = Bot(token=bot_token)
 bot.send_message(chat_id=chat_id, text=f"🌤 Morning Clothing Advice 🌤\n\n{advice}")
+
+#web service
+
+from flask import Flask
+import threading
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running!"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run_web).start()
